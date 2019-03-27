bd050158c770a30d5348d6dbfaff73b40c318b5d
Jin Gi KONG <jgkong@kr.ibm.com>
2019-03-27 17:14:11 +0000

    Implement esp32 RMT TX
    r = machine.RMT(rmt_mode=machine.RMT.TX, channel=1, clk_div=80, pin=machine.Pin(16))
    r.config()
    r.send([((9000, 1), (4500, 0)), ((560, 1), (560, 0)), ((1690, 1), (560, 0)), ((560, 1), (0, 0))])

--- ports/esp32/machine_rmt.c
+++ ports/esp32/machine_rmt.c
@@ -44,14 +44,63 @@ typedef struct _esp32_rmt_obj_t {
     uint8_t buf_size;
 } esp32_rmt_obj_t;
 
-STATIC mp_obj_t esp32_rmt_make_new(const mp_obj_type_t *type,
-        size_t n_args, size_t n_kw, const mp_obj_t *args) {
-    mp_arg_check_num(n_args, n_kw, 1, MP_OBJ_FUN_ARGS_MAX, true);
+STATIC void esp32_rmt_config_helper(esp32_rmt_obj_t *self,
+        size_t n_args, const mp_obj_t *pos_args, mp_map_t *kw_args) {
+
+    const mp_arg_t *allowed_args;
+    static const mp_arg_t tx_allowed_args[] = {
+        { MP_QSTR_loop_en, MP_ARG_BOOL, {.u_int = false} },
+        { MP_QSTR_carrier_freq_hz, MP_ARG_INT, {.u_int = 38000} },
+        { MP_QSTR_carrier_duty_percent, MP_ARG_INT, {.u_int = 50} },
+        { MP_QSTR_carrier_level, MP_ARG_INT, {.u_int = RMT_CARRIER_LEVEL_HIGH} },
+        { MP_QSTR_carrier_en, MP_ARG_BOOL, {.u_int = true} },
+        { MP_QSTR_idle_level, MP_ARG_INT, {.u_int = RMT_IDLE_LEVEL_LOW} },
+        { MP_QSTR_idle_output_en, MP_ARG_BOOL, {.u_int = false} },
+    };
+    static const mp_arg_t rx_allowed_args[] = {
+    };
+
+    allowed_args = (self->config.rmt_mode == RMT_MODE_TX) ? tx_allowed_args : rx_allowed_args;
+
+    mp_arg_val_t args[MP_ARRAY_SIZE(allowed_args)];
+    mp_arg_parse_all(n_args, pos_args, kw_args,
+        MP_ARRAY_SIZE(allowed_args), allowed_args, args);
+
+    rmt_config(&self->config);
+    // TODO: argument check (0, 0)
+    rmt_driver_install(self->config.channel, 0, 0);
+}
+
+STATIC mp_obj_t esp32_rmt_make_new(const mp_obj_type_t *type, size_t n_args, size_t n_kw, const mp_obj_t *all_args) {
+    enum { ARG_rmt_mode, ARG_channel, ARG_clk_div, ARG_pin, ARG_mem_block_num };
+    static const mp_arg_t allowed_args[] = {
+        { MP_QSTR_rmt_mode, MP_ARG_REQUIRED | MP_ARG_INT, {.u_int = RMT_MODE_TX} },
+        { MP_QSTR_channel, MP_ARG_REQUIRED | MP_ARG_INT, {.u_int = 1} },
+        { MP_QSTR_clk_div, MP_ARG_REQUIRED | MP_ARG_INT, {.u_int = 100} },
+        { MP_QSTR_pin, MP_ARG_REQUIRED | MP_ARG_OBJ, {.u_obj = MP_OBJ_NULL} },
+        { MP_QSTR_mem_block_num, MP_ARG_KW_ONLY | MP_ARG_INT, {.u_int = 1} },
+    };
+
+    mp_arg_val_t args[MP_ARRAY_SIZE(allowed_args)];
+    mp_arg_parse_all_kw_array(n_args, n_kw, all_args, MP_ARRAY_SIZE(allowed_args), allowed_args, args);
+
     esp32_rmt_obj_t *self = m_new_obj(esp32_rmt_obj_t);
     self->base.type = &machine_rmt_type;
-    self->buf_size = mp_obj_get_int(args[0]);
+    self->config.rmt_mode = args[ARG_rmt_mode].u_int;
+    self->config.channel = args[ARG_channel].u_int;
+    self->config.clk_div = args[ARG_clk_div].u_int;
+    self->config.gpio_num = machine_pin_get_id(args[ARG_pin].u_obj);
+    self->config.mem_block_num = args[ARG_mem_block_num].u_int;
+
     return MP_OBJ_FROM_PTR(self);
 }
+
+STATIC mp_obj_t esp32_rmt_config(size_t n_args,
+        const mp_obj_t *args, mp_map_t *kw_args) {
+    esp32_rmt_config_helper(args[0], n_args - 1, args + 1, kw_args);
+    return mp_const_none;
+}
+MP_DEFINE_CONST_FUN_OBJ_KW(esp32_rmt_config_obj, 1, esp32_rmt_config);
 
 STATIC void esp32_rmt_print(const mp_print_t *print, mp_obj_t self_in,
         mp_print_kind_t kind) {
@@ -59,42 +108,24 @@ STATIC void esp32_rmt_print(const mp_print_t *print, mp_obj_t self_in,
     mp_printf(print, "RMT(Buffer Size: %u)", self->buf_size);
 }
 
-STATIC mp_obj_t esp32_rmt_tx_init(mp_obj_t self_in) {
-    //esp32_rmt_obj_t *self = self_in;
-    rmt_config_t rmt_tx;
-    rmt_tx.channel = 1;
-    rmt_tx.gpio_num = 16;
-    rmt_tx.mem_block_num = 1;
-    rmt_tx.clk_div = 100;
-    rmt_tx.tx_config.loop_en = false;
-    rmt_tx.tx_config.carrier_duty_percent = 50;
-    rmt_tx.tx_config.carrier_freq_hz = 38000;
-    rmt_tx.tx_config.carrier_level = 1;
-    rmt_tx.tx_config.carrier_en = 1;
-    rmt_tx.tx_config.idle_level = 0;
-    rmt_tx.tx_config.idle_output_en = true;
-    rmt_tx.rmt_mode = 0;
-    rmt_config(&rmt_tx);
-    rmt_driver_install(rmt_tx.channel, 0, 0);
-    return mp_const_none;
-}
-MP_DEFINE_CONST_FUN_OBJ_1(esp32_rmt_tx_init_obj, esp32_rmt_tx_init);
+STATIC mp_obj_t esp32_rmt_tx_send_cmd(mp_obj_t self_in, mp_obj_t items_in) {
+    esp32_rmt_obj_t *self = MP_OBJ_FROM_PTR(self_in);
+    int channel = self->config.channel;
+    size_t item_len;
+    mp_obj_t *items_arr;
+    mp_obj_get_array(items_in, &item_len, &items_arr);
+    mp_obj_t *val, *pulse;
 
-STATIC mp_obj_t esp32_rmt_tx_send_cmd(mp_obj_t self_in, mp_obj_t cmds_in) {
-    //esp32_rmt_obj_t *self = self_in;
-    int channel = 1;
-    size_t cmd_len;
-    mp_obj_t *cmds;
-    mp_obj_get_array(cmds_in, &cmd_len, &cmds);
-
-    size_t item_len = cmd_len / 2;
     size_t item_size = (sizeof(rmt_item32_t) * item_len);
     rmt_item32_t* items = (rmt_item32_t*) malloc(item_size);
     for (int i = 0; i < item_len; i++) {
-        items[i].level0 = 1;
-        items[i].level1 = 0;
-        items[i].duration0 = mp_obj_get_int(cmds[i*2]);
-        items[i].duration1 = mp_obj_get_int(cmds[i*2+1]);
+        mp_obj_get_array_fixed_n(items_arr[i], 2, &val);
+        mp_obj_get_array_fixed_n(val[0], 2, &pulse);
+        items[i].duration0 = mp_obj_get_int(pulse[0]);
+        items[i].level0 = mp_obj_get_int(pulse[1]);
+        mp_obj_get_array_fixed_n(val[1], 2, &pulse);
+        items[i].duration1 = mp_obj_get_int(pulse[0]);
+        items[i].level1 = mp_obj_get_int(pulse[1]);
     }
     rmt_write_items(channel, items, item_len, true);
     rmt_wait_tx_done(channel, portMAX_DELAY);
@@ -105,8 +136,11 @@ STATIC mp_obj_t esp32_rmt_tx_send_cmd(mp_obj_t self_in, mp_obj_t cmds_in) {
 MP_DEFINE_CONST_FUN_OBJ_2(esp32_rmt_tx_send_cmd_obj, esp32_rmt_tx_send_cmd);
 
 STATIC const mp_rom_map_elem_t esp32_rmt_locals_dict_table[] = {
-    { MP_ROM_QSTR(MP_QSTR_install), MP_ROM_PTR(&esp32_rmt_tx_init_obj) },
+    { MP_ROM_QSTR(MP_QSTR_config), MP_ROM_PTR(&esp32_rmt_config_obj) },
     { MP_ROM_QSTR(MP_QSTR_send), MP_ROM_PTR(&esp32_rmt_tx_send_cmd_obj) },
+
+    { MP_ROM_QSTR(MP_QSTR_RX), MP_ROM_INT(RMT_MODE_RX) },
+    { MP_ROM_QSTR(MP_QSTR_TX), MP_ROM_INT(RMT_MODE_TX) },
 };
 
 STATIC MP_DEFINE_CONST_DICT(esp32_rmt_locals_dict, esp32_rmt_locals_dict_table);
